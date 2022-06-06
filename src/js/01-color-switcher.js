b878885301f30b3078feca54b93335ff8cc4cef2
khodetsky <hodyaya@gmail.com>
2022-06-07 00:44:37 +0300

    done task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+
+stopBtn.setAttribute("disabled", "disabled");
+
+function startedToChangeBodyColor() {
+    startBtn.setAttribute("disabled", "disabled");
+    stopBtn.removeAttribute("disabled");
+    return changeBodyColorInterval = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`
+  }, 1000);
+};
+
+function stoppedChangingBodyColor() {
+    clearInterval(changeBodyColorInterval);
+    stopBtn.setAttribute("disabled", "disabled");
+    startBtn.removeAttribute("disabled");
+}
+
+stopBtn.addEventListener("click", stoppedChangingBodyColor);
+startBtn.addEventListener("click", startedToChangeBodyColor);
 
